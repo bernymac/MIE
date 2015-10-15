cf0c688b8eb4180a69bd53cf79f1393f25ccfeb0
Bernardo Ferreira <p110397@campus.fct.unl.pt>
2015-10-15 14:47:37 +0100

    Added command line parametrization for server selection.

--- Server/Main.cpp
+++ Server/Main.cpp
@@ -11,8 +11,16 @@
 #include "CashServer.hpp"
 
 int main(int argc, const char * argv[]) {
-//    MIEServer server;
-//    SSEServer server;
-    CashServer server;
+    if (argc != 2)
+        printf("Incorrect number of arguments. Please pass only a server name, e.g. \"MIE\", \"SSE\" or \"Cash\"\n");
+    else
+        if (strcasecmp(argv[1], "MIE"))
+            MIEServer server;
+        else
+            if (strcasecmp(argv[1], "SSE"))
+                SSEServer server;
+            else
+                if (strcasecmp(argv[1], "Cash"))
+                    CashServer server;
 }
 
