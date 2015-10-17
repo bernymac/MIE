aa315f1b61e994556726b2eb832d6167b597dd5a
Bernardo Ferreira <p110397@campus.fct.unl.pt>
2015-10-17 12:48:59 +0100

    -

--- Server/Main.cpp
+++ Server/Main.cpp
@@ -12,17 +12,20 @@
 #include "CashServer.hpp"
 
 int main(int argc, const char * argv[]) {
-    if (argc != 2)
+/*    if (argc != 2)
         printf("Incorrect number of arguments. Please give a server name, e.g. \"MIE\", \"SSE\" or \"Cash\"\n");
-    else if (strcasecmp(argv[1], "-mie") == 0)
+    else if (strcasecmp(argv[1], "mie") == 0)
         MIEServer server;
-    else if (strcasecmp(argv[1], "-mieMT") == 0)
+    else if (strcasecmp(argv[1], "mieMT") == 0)
         MIEServerMT server;
-    else if (strcasecmp(argv[1], "-sse") == 0)
+    else if (strcasecmp(argv[1], "sse") == 0)
         SSEServer server;
-    else if (strcasecmp(argv[1], "-cash") == 0)
+    else if (strcasecmp(argv[1], "cash") == 0)
         CashServer server;
     else
         printf("Server command not recognized! Available Servers: \"mie\", \"mieMT\", \"sse\" and \"Cash\"\n");
+ */
+    
+    CashServer server;
 }
 
