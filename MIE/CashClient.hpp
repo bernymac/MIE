ec03ba9d243bc11abc97d61c3ad2866eae27b450
Bernardo Ferreira <bernardolferreira@gmail.com>
2016-04-18 15:57:56 +0100

    -

--- MIE/CashClient.hpp
+++ MIE/CashClient.hpp
@@ -15,7 +15,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/features2d/features2d.hpp>
-#include <opencv2/xfeatures2d/nonfree.hpp>
+//#include <opencv2/xfeatures2d/nonfree.hpp>
 #include <set>
 #include "EnglishAnalyzer.h"
 #include "CashCrypt.hpp"
