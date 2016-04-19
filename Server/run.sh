7d4001ffe1a2facd96b0b6832c9f5f0007b7b0e6
Bernardo Ferreira <bernardolferreira@gmail.com>
2016-04-19 15:12:17 +0100

    Added run script.

---
+++ Server/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#  run.sh
+#  MIE
+#
+#  Created by Bernardo Ferreira on 19/04/16.
+#  Copyright © 2016 NovaSYS. All rights reserved.
+
+export LD_LIBRARY_PATH=/usr/local/opencv/opencv3.1/lib/:$LD_LIBRARY_PATH
+./Server mie
