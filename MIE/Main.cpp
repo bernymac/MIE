799d1c419e9dca7a2f53c423e12ff45736f28ac6
Bernardo Ferreira <bernardolferreira@gmail.com>
2016-04-26 09:24:10 +0100

    -

--- MIE/Main.cpp
+++ MIE/Main.cpp
@@ -29,9 +29,9 @@ void runMIEClient() {
     MIEClient mie;
     timespec start = getTime();
     
-    mie.addDocs("inriaHolidays","flickr_tags",1,1491,0);
+//    mie.addDocs("inriaHolidays","flickr_tags",1,1491,0);
 //    mie.addDocs("flickr_imgs", "flickr_tags",1,1000,0);
-    mie.index();
+//    mie.index();
     
 /*    string imgPath = homePath;
     imgPath += "Datasets/inriaHolidays/100701.jpg";
@@ -43,7 +43,7 @@ void runMIEClient() {
     LOGI("%s total_time:%.6f\n",mie.printTime().c_str(),total_time);
 //    printQueryResults(queryResults);
 
-/*    map<int,vector<QueryResult> > queries;
+    map<int,vector<QueryResult> > queries;
     char* imgPath = new char[120];
     char* textPath = new char[120];
     for (int i = 100000; i <= 149900; i+=100) {
@@ -66,7 +66,7 @@ void runMIEClient() {
     string fName = homePath;
     fName += "Data/Client/MIE/mieHoliday.dat";
     printHolidayResults(fName, queries);
-*/
+
 }
 
 void runSSEClient() {
